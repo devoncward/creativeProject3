0972782dac106f4529fd72add6ca215920462157
Devon Ward <devoncward@gmail.com>
2019-05-28 17:40:09 -0600

    all apis now working

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ var app = new Vue ({
   el: '#app',
   data: {
     geoLocation: '',
+    geoLat: '',
+    geoLong: '',
     backgroundImg: '',
+    hikes: {},
   },
   created() {
     this.getMountainImage();
+    this.getLongLat();
   },
   methods: {
     async getMountainImage() {
@@ -13,13 +17,31 @@ var app = new Vue ({
         const url = "https://source.unsplash.com/1600x900/?outdoors";
         const response = await axios.get(url)
         const data = response.config.url
-        backgroundImg = data;
+        this.backgroundImg = data;
         console.log("Response: ", response);
         console.log(data);
       } catch (error) {
         console.log(error);
       }
     },
+    async getLongLat() {
+      const url = "http://api.openweathermap.org/data/2.5/weather?q=" + "Provo" + ",US&units=imperial" + "&APPID=cf3be95cf53b4267405bcea8c73696ee";
+      const response = await fetch(url);
+      const json = await response.json();
+      console.log(json);
+      this.geoLong = json.coord.lon;
+      this.geoLat = json.coord.lat;
+      console.log("latitude: ", this.geoLat, " longitude: ", this.geoLong);
+      this.getHikingTrails();
+    },
+    async getHikingTrails() {
+      const url = "https://www.hikingproject.com/data/get-trails?lat=" + this.geoLat + "&lon=" + this.geoLong + "&maxDistance=20&key=200478900-f017ed4af606d3aa2ca01907336330f4";
+      console.log(url);
+      const response = await fetch(url);
+      const json = await response.json();
+      console.log(json);
+
+    }
   },
   computed: {
 
